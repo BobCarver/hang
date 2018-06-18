4bd94d1356d0112ebfe4cae81f8edd587ad9de1e
Bob Carver <bob.m.carver@gmail.com>
2018-06-18 17:40:16 -0500

    extend index to support ^c and ignore non-lowercase letters

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ const Word = require('./word')
 keypress(process.stdin);
 
 function randomWord() { return ['one','two','three'][Math.floor(Math.random() * 3)]}
+// Esc[2J to clear screen
 
 var game = new Word(randomWord())
 console.log(game.toString());
 
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
-    if( ! game.guessed() ) {
-        game.guess(ch)
-        console.log(game.toString());
-        if ( game. guessed() ) console.log('You won another game (y/n)?')
+    if (key && key.ctrl ) {
+        if(key.name == 'c') process.stdin.pause();
+    } else if( ! game.guessed() ) {
+        if ('abcdefghijklmnopqurstuvwxyz'.includes(ch) ) {
+            game.guess(ch)
+            console.log(game.toString());
+            if ( game. guessed() ) console.log('You won!  Another game (y/n)?')
+        }
     } else {
         if(  ch != 'y') process.exit();
         console.log((game = new Word(randomWord())).toString())
